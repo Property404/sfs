a31f50242c7147ba0b62078b35c7a7208f165506
Property404 <daganboy@gmail.com>
2017-11-03 18:26:56 -0400

    Pystasm stuff

---
+++ get_face_points.py
@@ -0,0 +1,27 @@
+import os.path
+import cv2
+import stasm
+
+def get_face_points(image_name):
+	path = os.path.join(stasm.DATADIR, image_name)
+
+	img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+	if img is None:
+		print("Cannot load", path)
+		raise SystemExit
+
+	landmarks = stasm.search_single(img)
+
+	if len(landmarks) == 0:
+		print("No face found in", path)
+	else:
+		landmarks = stasm.force_points_into_image(landmarks, img)
+		for point in landmarks:
+			img[round(point[1])][round(point[0])] = 255
+
+	cv2.imshow("stasm minimal", img)
+	cv2.waitKey(0)
+
+if __name__=="__main__":
+	get_face_points("bla.jpg");
